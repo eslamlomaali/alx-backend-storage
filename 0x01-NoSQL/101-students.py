9f41d48814110daeae649b6d005d6c0c5034e662
eslamlomaali <eslam@gmail.com>
2024-06-14 13:14:58 +0000

    done

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+""" MongoDB Operations with Python by  pymongo """
+
+
+def top_students(mongo_collection):
+    # sourcery skip: inline-immediately-returned-variable
+    """ Returns all students sorted """
+    top_student = mongo_collection.aggregate([
+        {
+            "$project": {
+                "name": "$name",
+                "averageScore": {"$avg": "$topics.score"}
+            }
+        },
+        {"$sort": {"averageScore": -1}}
+    ])
+
+    return top_student
